94791308f4a2cfd767687bfa738bbffe26d37937
Logan McNaughton <logan@bacoosta.com>
2021-10-19 21:52:14 -0600

    update format

--- pool-check.py
+++ pool-check.py
@@ -24,76 +24,80 @@ class LokiOutput(TypedDict):
 def get_metrics() -> List[Dict[str, Union[str, bool, int]]]:
     metrics = []
     with requests.Session() as s:
-        retries = Retry(total=5, backoff_factor=0.5, status_forcelist=Retry.RETRY_AFTER_STATUS_CODES)
-        s.mount('https://', HTTPAdapter(max_retries=retries))
+        retries = Retry(
+            total=5, backoff_factor=0.5, status_forcelist=Retry.RETRY_AFTER_STATUS_CODES
+        )
+        s.mount("https://", HTTPAdapter(max_retries=retries))
         try:
-            capacity = s.get('https://www.ymcacalgary.org/capacity/')
+            capacity = s.get("https://www.ymcacalgary.org/capacity/")
             if capacity.status_code != 200:
                 print("HTTP error in GET capacity: " + capacity.text)
                 return []
             soup = BeautifulSoup(capacity.content, "html.parser")
-            table = soup.find_all('table')[0]
-            table_body = table.find('tbody')
-            rows = table_body.find_all('tr')
+            table = soup.find_all("table")[0]
+            table_body = table.find("tbody")
+            rows = table_body.find_all("tr")
             for row in rows:
-                cols = row.find_all('td')
+                cols = row.find_all("td")
                 if cols:
                     ymca: Dict[str, Union[str, bool, int]] = {}
                     metrics.append(ymca)
-                    divs = cols[1].find_all('div')
+                    divs = cols[1].find_all("div")
                     ymca["name"] = cols[0].text
                     if len(divs) > 0:
-                        ymca['status'] = divs[0]['id']
+                        ymca["status"] = divs[0]["id"]
                     else:
-                        ymca['status'] = cols[1]['id']
-            script = soup.find_all('script')[3]
+                        ymca["status"] = cols[1]["id"]
+            script = soup.find_all("script")[3]
             script = str(script)
             for ymca in metrics:
-                pos = script.find("#" + str(ymca['status']))
+                pos = script.find("#" + str(ymca["status"]))
                 pos = script.find("addClass(", pos)
-                ymca['status'] = script[pos:].split('"')[1]
+                ymca["status"] = script[pos:].split('"')[1]
 
-            hours = s.get('https://www.ymcacalgary.org/faqs/')
+            hours = s.get("https://www.ymcacalgary.org/faqs/")
             if hours.status_code != 200:
                 print("HTTP error in GET hours: " + hours.text)
                 return []
             soup = BeautifulSoup(hours.content, "html.parser")
-            table = soup.find_all('table')[1]
-            rows = table.find_all('tr')
+            table = soup.find_all("table")[1]
+            rows = table.find_all("tr")
             for row in rows:
-                cols = row.find_all('td')
+                cols = row.find_all("td")
                 if cols:
                     for ymca in metrics:
-                        if str(ymca['name'])[:8] in cols[0].text:
-                            mf = cols[1].text.split(' - ')
-                            sat = cols[2].text.split(' - ')
+                        if str(ymca["name"])[:8] in cols[0].text:
+                            mf = cols[1].text.split(" - ")
+                            sat = cols[2].text.split(" - ")
                             if "Closed Sunday" in sat[1]:
-                                sat[1] = sat[1].split('\r\n')[0]
+                                sat[1] = sat[1].split("\r\n")[0]
                                 sun = ["12:00am", "12:00am"]
                             else:
                                 sun = sat
-                            today = dateutil.utils.today(tzinfo=gettz("America/Edmonton"))
+                            today = dateutil.utils.today(
+                                tzinfo=gettz("America/Edmonton")
+                            )
                             weekday = today.weekday()
                             if weekday < 5:
-                                open_hour = mf[0].split(':')[0]
-                                open_minute = mf[0].split(':')[1]
-                                close_hour = mf[1].split(':')[0]
-                                close_minute = mf[1].split(':')[1]
+                                open_hour = mf[0].split(":")[0]
+                                open_minute = mf[0].split(":")[1]
+                                close_hour = mf[1].split(":")[0]
+                                close_minute = mf[1].split(":")[1]
                             elif weekday == 5:
-                                open_hour = sat[0].split(':')[0]
-                                open_minute = sat[0].split(':')[1]
-                                close_hour = sat[1].split(':')[0]
-                                close_minute = sat[1].split(':')[1]
+                                open_hour = sat[0].split(":")[0]
+                                open_minute = sat[0].split(":")[1]
+                                close_hour = sat[1].split(":")[0]
+                                close_minute = sat[1].split(":")[1]
                             else:
-                                open_hour = sun[0].split(':')[0]
-                                open_minute = sun[0].split(':')[1]
-                                close_hour = sun[1].split(':')[0]
-                                close_minute = sun[1].split(':')[1]
+                                open_hour = sun[0].split(":")[0]
+                                open_minute = sun[0].split(":")[1]
+                                close_hour = sun[1].split(":")[0]
+                                close_minute = sun[1].split(":")[1]
                             open_hour = int(open_hour)
                             close_hour = int(close_hour)
-                            if 'pm' in open_minute:
+                            if "pm" in open_minute:
                                 open_hour += 12
-                            if 'pm' in close_minute:
+                            if "pm" in close_minute:
                                 close_hour += 12
                             open_minute = open_minute[:-2]
                             close_minute = close_minute[:-2]
@@ -101,7 +105,10 @@ def get_metrics() -> List[Dict[str, Union[str, bool, int]]]:
                             close_minute = int(close_minute)
                             open = today.replace(hour=open_hour, minute=open_minute)
                             close = today.replace(hour=close_hour, minute=close_minute)
-                            if time.time() < close.timestamp() and time.time() > open.timestamp():
+                            if (
+                                time.time() < close.timestamp()
+                                and time.time() > open.timestamp()
+                            ):
                                 ymca["open"] = True
                             else:
                                 ymca["status"] = "red"
@@ -109,33 +116,39 @@ def get_metrics() -> List[Dict[str, Union[str, bool, int]]]:
                             ymca["open_time"] = open.isoformat()
                             ymca["close_time"] = close.isoformat()
             for ymca in metrics:
-                if ymca['status'] == 'green':
-                    ymca['capacity'] = 0
-                elif ymca['status'] == 'yellow':
-                    ymca['capacity'] = 1
-                elif ymca['status'] == 'red':
-                    ymca['capacity'] = 2
+                if ymca["status"] == "green":
+                    ymca["capacity"] = 0
+                elif ymca["status"] == "yellow":
+                    ymca["capacity"] = 1
+                elif ymca["status"] == "red":
+                    ymca["capacity"] = 2
         except (requests.exceptions.RequestException, OSError) as e:
             print("HTTP error in GET: %s" % e)
             return []
     return metrics
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with requests.Session() as s_loki:
-        retries = Retry(total=5, backoff_factor=0.5, status_forcelist=Retry.RETRY_AFTER_STATUS_CODES)
-        s_loki.mount('https://', HTTPAdapter(max_retries=retries))
-        s_loki.auth = (os.environ.get('LOKI_USER', ''), os.environ.get('LOKI_PASS', ''))
-        loki_output: LokiOutput = {'streams': []}
-        item_output: ItemOutput = {'stream': {}, 'values': []}
-        item_output['stream']['job'] = 'ymca_pools'
+        retries = Retry(
+            total=5, backoff_factor=0.5, status_forcelist=Retry.RETRY_AFTER_STATUS_CODES
+        )
+        s_loki.mount("https://", HTTPAdapter(max_retries=retries))
+        s_loki.auth = (os.environ.get("LOKI_USER", ""), os.environ.get("LOKI_PASS", ""))
+        loki_output: LokiOutput = {"streams": []}
+        item_output: ItemOutput = {"stream": {}, "values": []}
+        item_output["stream"]["job"] = "ymca_pools"
         metrics = get_metrics()
         for ymca in metrics:
-            item_output['values'].append([str(time.time_ns()), json.dumps(ymca)])
-        loki_output['streams'].append(item_output)
-        if item_output['values']:
+            item_output["values"].append([str(time.time_ns()), json.dumps(ymca)])
+        loki_output["streams"].append(item_output)
+        if item_output["values"]:
             try:
-                r = s_loki.post('https://logs-prod-us-central2.grafana.net/loki/api/v1/push', json=loki_output, timeout=20)
+                r = s_loki.post(
+                    "https://logs-prod-us-central2.grafana.net/loki/api/v1/push",
+                    json=loki_output,
+                    timeout=20,
+                )
                 if r.status_code != 200 and r.status_code != 204:
                     print(str(datetime.now()) + " Loki error: " + r.text)
                     for ymca in metrics:
